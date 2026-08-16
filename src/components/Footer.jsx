8abdd9a82c8997c81a03b3c410164315f5f1c4fa
Sopan Reddy <nadagudesopan@gmail.com>
2026-08-16 19:35:11 +0530

    ..

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { ArrowUpRight, ArrowUp, Instagram } from "lucide-react";
+import { ArrowUp, Instagram } from "lucide-react";
 
 function Footer() {
 
