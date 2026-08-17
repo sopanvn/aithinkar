7aba84cac51fa14dd1eac06d3afe3820ff4ca291
Sopan Reddy <nadagudesopan@gmail.com>
2026-08-17 19:10:05 +0530

    ..

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -11,7 +11,6 @@ import {
 } from "lucide-react";
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import "../contact.css";
 
 const services = [
   "Website Development",
